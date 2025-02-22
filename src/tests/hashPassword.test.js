10192f1ee6d8991eead9d05d539821ccba82b4b3
appsy-gc <167824281+appsy-gc@users.noreply.github.com>
2025-02-22 15:43:22 +1000

    Added more tests to hashPassword.test

--- src/tests/hashPassword.test.js
+++ src/tests/hashPassword.test.js
@@ -1,15 +1,38 @@
+/* eslint-disable no-underscore-dangle */
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-const User = require('../models/userTest');
+const hashPasswordPlugin = require('../middleware/hashPassword');
 
+// Create user model for test
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.plugin(hashPasswordPlugin, { saltRounds: 10 });
+
+const User = mongoose.model('User', userSchema);
+
+// Mock bcrypt
 jest.mock('bcrypt');
 
+// Tests for hashPassword function
 describe('hashPassword plugin', () => {
   beforeAll(async () => {
     await mongoose.connect('mongodb://localhost:27017/test_db');
   });
   afterAll(async () => {
     await mongoose.connection.close();
+    jest.clearAllMocks();
+  });
+
+  beforeEach(() => {
     jest.clearAllMocks();
   });
 
@@ -25,5 +48,35 @@ describe('hashPassword plugin', () => {
     expect(bcrypt.hash).toHaveBeenCalledTimes(1);
     expect(bcrypt.hash).toHaveBeenCalledWith('plainTextPassword', 10);
     expect(user.password).toBe(mockedHash);
+  });
+
+  test('Should not re-hash if the password is not modified', async () => {
+    bcrypt.hash.mockResolvedValue('firstHash');
+
+    const existingUser = new User({
+      username: 'user_name',
+      password: 'plainTextPassword',
+    });
+
+    await existingUser.save();
+    bcrypt.hash.mockClear();
+    const user = await User.findById(existingUser._id);
+    await user.save();
+
+    expect(bcrypt.hash).not.toHaveBeenCalled();
+    expect(user.password).toBe('firstHash');
+  });
+
+  test('Should call next(error) if bcrypt.hash failes', async () => {
+    const testError = new Error('bcrypt error');
+    bcrypt.hash.mockRejectedValue(testError);
+
+    const user = new User({
+      username: 'error_test',
+      password: 'plainTextPassword',
+    });
+
+    await expect(user.save()).rejects.toThrow('bcrypt error');
+    expect(bcrypt.hash).toHaveBeenCalledTimes(1);
   });
 });
